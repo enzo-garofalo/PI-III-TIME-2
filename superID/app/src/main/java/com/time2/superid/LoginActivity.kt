d019498d59c2cf5c36490e86fbbb6f5010bd6425
enzo-garofalo <dev.enzo.garofalo@gmail.com>
2025-03-26 19:40:59 -0300

    Super ID

--- superID/app/src/main/java/com/time2/superid/LoginActivity.kt
+++ superID/app/src/main/java/com/time2/superid/LoginActivity.kt
@@ -7,8 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,7 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.time2.superid.ui.theme.SuperIDTheme
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
