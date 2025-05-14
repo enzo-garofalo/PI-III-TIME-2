b1c8dcb3d46e6ca1b535baf67cb7f8b642a1d5df
enzo-garofalo <dev.enzo.garofalo@gmail.com>
2025-05-13 22:54:35 -0300

    fix: colors config and email text fiels

--- superID/app/src/main/java/com/time2/superid/ui/theme/Type.kt
+++ superID/app/src/main/java/com/time2/superid/ui/theme/Type.kt
@@ -24,6 +24,13 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
+    titleSmall = TextStyle(
+        fontFamily = urbanist,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
     titleMedium = TextStyle(
         fontFamily = urbanist,
         fontWeight = FontWeight.Normal,
