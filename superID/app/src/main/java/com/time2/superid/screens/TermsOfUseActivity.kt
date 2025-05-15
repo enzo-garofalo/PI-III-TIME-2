77a33e744d8394f03be420af932f65aef6be027a
jgabrieldsl <jgabriel.leite@outlook.com>
2025-05-15 11:05:52 -0300

    feat: terms of use & conditions finished

--- superID/app/src/main/java/com/time2/superid/screens/TermsOfUseActivity.kt
+++ superID/app/src/main/java/com/time2/superid/screens/TermsOfUseActivity.kt
@@ -4,17 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,7 +20,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -41,9 +37,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.time2.superid.AccountsHandler.SignUpActivity
-import com.time2.superid.LoginActivity
 import com.time2.superid.R
-import com.time2.superid.utils.showShortToast
 
 class TermsOfUseActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -94,17 +88,150 @@ fun TermsOfUseView(modifier: Modifier = Modifier) {
             )
         }
 
-        // Título
-        Text(
-            text = "Termos e Condições\nde Uso do SuperID",
-            style = TextStyle(
-                fontSize = 30.sp,
-                lineHeight = 39.sp,
-                fontFamily = FontFamily(Font(R.font.urbanist)),
-                fontWeight = FontWeight(700),
-                color = Color(0xFF1E232C),
+        // Column para diferenciar o espaço
+        Column(
+            verticalArrangement = Arrangement.spacedBy(23.dp, Alignment.Top),
+            horizontalAlignment = Alignment.Start,
+        ) {
+            // Termos e Condições de Uso do SuperID
+            Text(
+                text = "Termos e Condições\nde Uso do SuperID",
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    lineHeight = 39.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF1E232C),
                 )
-        )
+            )
+
+            Text(
+                text = "Bem-vindo ao SuperID, um aplicativo de gerenciamento de senhas desenvolvido para proteger suas credenciais digitais com segurança e praticidade. Ao acessar ou utilizar o SuperID, você concorda em cumprir e estar vinculado aos presentes Termos de Uso (\"Termos\"). Se você não concordar com estes Termos, por favor, não utilize o aplicativo.",
+                style = TextStyle (
+                    fontSize = 15.sp,
+                    lineHeight = 22.5.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF8391A1),
+                )
+            )
+
+            // 1
+            Text(
+                text = "1. Aceitação dos Termos",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF1E232C),
+                    textAlign = TextAlign.Center,
+                )
+            )
+
+            Text(
+                text = "Ao acessar ou usar o SuperID, você confirma que:\nLeu e aceita estes Termos e a Política de Privacidade.\nTem 16 anos ou mais.\nFornecerá informações precisas ao criar sua conta.",
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    lineHeight = 22.5.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF8391A1),
+
+                    )
+            )
+
+            // 2
+            Text(
+                text = "2. Uso do Serviço",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF1E232C),
+                    textAlign = TextAlign.Center,
+                )
+            )
+
+            Text(
+                text = "O SuperID permite armazenar senhas em um cofre criptografado, gerar senhas fortes e sincronizar dados entre dispositivos. Você concorda em: Usar o aplicativo apenas para fins legais. Proteger sua senha mestra e ativar autenticação multifator. Não realizar engenharia reversa ou acessar dados de outros usuários.",
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    lineHeight = 22.5.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF8391A1),
+                )
+            )
+
+            // 3
+            Text(
+                text = "3. Segurança e compromisso",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF1E232C),
+                    textAlign = TextAlign.Center,
+                )
+            )
+
+            Text(
+                text = "O SuperID usa criptografia AES-256 de conhecimento zero. Você é responsável por manter a segurança de sua conta e dispositivos. Faça backups regulares. O SuperID não se responsabiliza por perdas de dados devido a falhas de backup ou acesso não autorizado.",
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    lineHeight = 22.5.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF8391A1),
+                )
+            )
+
+            // 4
+            Text(
+                text = "4. Rescisão e Alterações",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF1E232C),
+                    textAlign = TextAlign.Center,
+                )
+            )
+
+            Text(
+                text = "Você pode encerrar sua conta a qualquer momento. Após a rescisão, seus dados serão excluídos, exceto backups locais. Podemos atualizar estes Termos e notificaremos você sobre mudanças significativas. O uso contínuo do SuperID implica aceitação das alterações.",
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    lineHeight = 22.5.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF8391A1),
+                )
+            )
+
+            // 5
+            Text(
+                text = "5. Contato",
+                style = TextStyle(
+                    fontSize = 24.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFF1E232C),
+                    textAlign = TextAlign.Center,
+                )
+            )
+
+            Text(
+                text = "Se você tiver dúvidas sobre estes Termos ou sobre o SuperID, entre em contato conosco:\n\nSuperID Tecnologia Ltda. \n E-mail: suporteSuperID@gmail.com\nEndereço: PUC Campinas Campus 1\nWebsite: superid.vercel.app\nPlaystore: SuperID\n\nAo clicar em \"Aceitar\" ou usar o SuperID, você confirma que leu e concorda com estes Termos de Uso e nossa Política de Privacidade.",
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    lineHeight = 22.5.sp,
+                    fontFamily = FontFamily(Font(R.font.urbanist)),
+                    fontWeight = FontWeight(500),
+                    color = Color(0xFF8391A1),
+                )
+            )
+        }
 
         Button(
             onClick = {context.startActivity(Intent(context, SignUpActivity::class.java))},
@@ -131,6 +258,5 @@ fun TermsOfUseView(modifier: Modifier = Modifier) {
                 modifier = Modifier.padding(vertical = 12.dp)
             )
         }
-
     }
 }
